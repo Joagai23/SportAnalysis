5a07b87929ed4d1635bfe1505d85090a212db143
Joagai23 <jaguirregomezcorta@gmail.com>
2023-05-06 14:23:20 +0000

    Reorder folders + Ready everything before training

---
+++ Scripts/log_writer.py
@@ -0,0 +1,13 @@
+# Append message to log file
+def write_log(message, file_name):
+
+    # Open file in append mode
+    file = open(file=file_name, mode="a")
+
+    # Write log
+    file.write(message)
+    # Write line jump
+    file.write("\n")
+
+    # Close file
+    file.close()
